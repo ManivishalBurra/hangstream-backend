139b6462dfedc7119b981b1de261c20dc0c8aeb5
manivishal <manivishalbits@gmail.com>
2021-06-13 21:00:34 +0530

    new movies schema for IMDb added

--- models/movie.js
+++ models/movie.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 const movieSchema = new mongoose.Schema({
         movieName:String,
         banner:String,
-        movieUrl:String
+        movieUrl:String,
+        ratings:String,
+        genres:[String],
+        plotOutline:String,
+        year:String,
+        movieID:String
     });
 
 module.exports = mongoose.model('Movie',movieSchema,"Movie")
