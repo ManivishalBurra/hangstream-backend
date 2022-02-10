81f47931d105ee75902d7a3479063b4440936fc0
ManivishalBurra <72870001+ManivishalBurra@users.noreply.github.com>
2022-02-11 02:40:32 +0530

    private_removed

--- models/movie.js
+++ models/movie.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
-        movieName:String,
-        banner:String,
-        movieUrl:String,
-        ratings:String,
-        genres:[String],
-        plotOutline:String,
-        year:String,
-        movieID:String
+    movieName:String,
+    banner:String,
+    movieUrl:[String],
+    ratings:String,
+    genres:[String],
+    plotOutline:String,
+    year:String,
+    movieID:String,
+    poster:String,
+    episode:[String]
     });
 
 module.exports = mongoose.model('Movie',movieSchema,"Movie")
--- routes/Home/room.js
+++ routes/Home/room.js
@@ -2,20 +2,22 @@ const app = require("express")();
 const User = require("../../models/user")
 const Movie = require("../../models/movie")
 app.post("/",(req,res)=>{
-    console.log(req.body);
     if(req.body.movieName!=="" && req.body.source==="admin"){
     const movie = new Movie({
       movieName:req.body.movieName,
       banner:req.body.banner,
-      movieUrl:req.body.url,
+      movieUrl:[req.body.url],
       ratings:req.body.ratings,
       genres:req.body.genres,
       movieID:req.body.movieID,
       plotOutline:req.body.plotOutline,
-      year:req.body.year
+      year:req.body.year,
+      poster:req.body.image,
+      episode:[req.body.movieName]
     });
     movie.save();
     }
+    console.log(req.body.url)
     User.findOneAndUpdate({id:req.body.id},
         {movieName:req.body.movieName,banner:req.body.banner,roomId:req.body.roomid,movieUrl:req.body.url},
         (err,data)=>{
